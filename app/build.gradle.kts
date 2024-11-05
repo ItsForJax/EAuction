2d5454d77292405bd2a5155fe795f5fad0638667
4jax <dadelos.ddls@gmail.com>
2024-11-06 00:54:56 +0800

    Added Serialization and Navigation requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,9 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
