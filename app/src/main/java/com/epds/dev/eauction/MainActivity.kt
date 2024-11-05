c8d04b52b7218d90d5875dbd5e805be104b0ec3b
4jax <dadelos.ddls@gmail.com>
2024-11-06 00:55:52 +0800

    Updated

--- app/src/main/java/com/epds/dev/eauction/MainActivity.kt
+++ app/src/main/java/com/epds/dev/eauction/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import com.epds.dev.eauction.ui.screens.seller.StartScreen
+import com.epds.dev.eauction.ui.screens.buyer.main.HomeScreen
 import com.epds.dev.eauction.ui.theme.AppTheme
-
 
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +38,21 @@ class MainActivity : ComponentActivity() {
                 systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
             AppTheme {
-                StartScreen()
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(WindowInsets.statusBars.asPaddingValues()) // Adds padding for the status bar
+                ) {
+                    HomeScreen(
+                        name = "Mr.Buyer",
+                        status = "Set Status",
+                        avatar = painterResource(R.drawable.avatar),
+                        modifier = Modifier.weight(1f)
+                    )
+                    Column (modifier = Modifier.fillMaxWidth().height(75.dp).background(Color.Red)) {
+                        Text("Sudo")
+                    }
+                }
             }
         }
 
